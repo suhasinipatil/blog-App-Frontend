432733469abd690bc78f68ba65bb0aa445bd5e34
suhasinipatil <suhasinibpatil09@gmail.com>
2023-12-04 17:40:28 +0530

    added edit and delete buttons

--- src/ArticleItem.js
+++ src/ArticleItem.js
@@ -1,16 +1,24 @@
 // Desc: This file contains the ArticleItem component which is used to display the article title, author, subtitle, body, and comments.
 
 import CreateComment from "./CreateComment";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "./AuthContext";
 
 const ArticleItem = ({props, comments}) => {
     const [articleComments, setArticleComments] = useState(comments); 
+    const { token , loggedIn } = useContext(AuthContext);
 
     const addComment = (newComment) => {
         // Add the new comment to the beginning of the comments list
         setArticleComments([newComment, ...comments]);
-      };
+    };
     
+    const handleDelete = (id) => {
+    };
+
+    const handleEdit = (id) => {
+    };
+
     return (
         <div className="articleItem">
             <div className="titleAuthor">
@@ -19,6 +27,10 @@ const ArticleItem = ({props, comments}) => {
             </div>
             <h3>{props.subtitle}</h3>
             <p className="articleBody">{props.body}</p>
+            <div className="actions">
+                <button className="editIcon" onClick={() => handleEdit(props.id)} >Edit</button>
+                <button className="deleteIcon" onClick={() => handleDelete(props.id)} >Delete</button>
+            </div>
             <h4 style={{ textAlign: 'left', marginLeft: '30px' }}>Comments:</h4>
             <div>
                 <CreateComment postId={props.id} addComment={addComment}/>
@@ -28,10 +40,8 @@ const ArticleItem = ({props, comments}) => {
                             {comment.body}
                         </li>
                     ))}
-                    
                 </ul>
             </div> 
-            
         </div>
     );
 }
